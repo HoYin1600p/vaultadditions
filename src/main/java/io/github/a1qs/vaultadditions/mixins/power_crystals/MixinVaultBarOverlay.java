bb4f02ba5f152b948532039f2ef7503224971b63
attack8 <attackeight@gmail.com>
2026-04-14 21:15:06 -0400

    Update for VH 3.21.4.1

--- src/main/java/io/github/a1qs/vaultadditions/mixins/power_crystals/MixinVaultBarOverlay.java
+++ src/main/java/io/github/a1qs/vaultadditions/mixins/power_crystals/MixinVaultBarOverlay.java
@@ -1,37 +1,43 @@
 package io.github.a1qs.vaultadditions.mixins.power_crystals;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import com.mojang.blaze3d.vertex.PoseStack;
 import io.github.a1qs.vaultadditions.util.MiscUtil;
 import iskallia.vault.client.gui.overlay.VaultBarOverlay;
-import iskallia.vault.client.render.IVaultOptions;
+import iskallia.vault.client.render.hud.module.VaultPointsModule;
+import iskallia.vault.client.render.hud.module.context.ModuleRenderContext;
+import iskallia.vault.init.ModOptions;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.player.LocalPlayer;
-import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.network.chat.Style;
 import net.minecraft.network.chat.TextColor;
 import net.minecraft.network.chat.TextComponent;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(value = VaultBarOverlay.class, remap = false)
-public class MixinVaultBarOverlay {
+@Mixin(value = VaultPointsModule.class, remap = false)
+public abstract class MixinVaultBarOverlay {
 
-    @Inject(method = "renderPointText", at = @At(value = "INVOKE", target = "Liskallia/vault/client/render/IVaultOptions;showPointMessages()Z"))
-    private static void renderPowerPoints(Minecraft minecraft, LocalPlayer player, PoseStack matrixStack, int right, MultiBufferSource.BufferSource buffer, CallbackInfo ci) {
-        minecraft.getProfiler().popPush("batchPowerPointText");
-        IVaultOptions options = (IVaultOptions) Minecraft.getInstance().options;
-        if (!options.showPointMessages() || MiscUtil.unspentPowerPoints == 0 || VaultBarOverlay.vaultLevel < 100) {
+    @Shadow
+    protected abstract int baseWidth(ModuleRenderContext context);
+
+    @Inject(method = "renderModule(Liskallia/vault/client/render/hud/module/context/ModuleRenderContext;)V", at = @At(value = "INVOKE", target = "Liskallia/vault/client/render/hud/module/context/ModuleRenderContext;isEditing()Z", ordinal = 1))
+    private void renderPowerPoints(ModuleRenderContext context, CallbackInfo ci, @Local(name = "poseStack") PoseStack poseStack, @Local(name = "rendered") boolean rendered, @Local(name = "y") int y) {
+        Minecraft.getInstance().getProfiler().popPush("batchPowerPointText");
+        if (!ModOptions.SHOW_POINT_MESSAGES.getValue() || MiscUtil.unspentPowerPoints == 0 || VaultBarOverlay.vaultLevel < 100) {
             return;
         }
 
+        int width = this.baseWidth(context) + 5;
+
         MiscUtil.POWER_POINT_SUPPLIER.ifChanged(MixinVaultBarOverlay::vaultadditions$onUnspentPowerPointsChanged);
-        float x = right - MiscUtil.unspentPowerPointComponentWidth - 5;
-        minecraft.font.drawInBatch(MiscUtil.unspentPowerPointComponent, x, 18.0F, 16777215, true, matrixStack.last().pose(), buffer, false, 0, 15728880);
-        matrixStack.translate(0.0, 12.0, 0.0);
+        Minecraft.getInstance().font.drawShadow(poseStack, MiscUtil.unspentPowerPointComponent, (float)(width - VaultBarOverlay.unspentArchetypePointComponentWidth), (float)y, 16777215);
+        y += 12;
+        rendered = true;
     }
 
     @Unique
